ccab1df319608a2e7ee13e8843de187c799b91d8
Артем Тюменцев <artemtumencev@MacBook-Pro-Artem.local>
2017-09-28 21:52:42 +0300

    Initial Commit

---
+++ new/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  new
+//
+//  Created by Артем Тюменцев on 28.09.17.
+//  Copyright © 2017 Артем Тюменцев. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ new/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  new
+//
+//  Created by Артем Тюменцев on 28.09.17.
+//  Copyright © 2017 Артем Тюменцев. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
