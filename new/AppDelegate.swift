41efaa99cd0d32f8ea651082b5193e87cc67d65e
Артем Тюменцев <artemtumencev@MacBook-Pro-Artem.local>
2017-09-28 23:44:38 +0300

    Обновил интерфейс 1.0

--- new/AppDelegate.swift
+++ new/AppDelegate.swift
@@ -13,14 +13,5 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
 
 
-    func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
-    }
-
-    func applicationWillTerminate(_ aNotification: Notification) {
-        // Insert code here to tear down your application
-    }
-
-
 }
 
